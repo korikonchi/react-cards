15d40b443b76070ad063315e9c7666232f40a241
KoriKonchi <korikonchi91@gmail.com>
2022-01-07 18:16:50 -0600

    "feat:update style"

--- src/App.js
+++ src/App.js
@@ -32,6 +32,7 @@ class App extends Component {
 
     return (
       <div className="App">
+        <h1>ROBOT ROLODEX</h1>
         <SearchBox
           placeholder={"search monster"}
           handleChange={this.handleChange}
